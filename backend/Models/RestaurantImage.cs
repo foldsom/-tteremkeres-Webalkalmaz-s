e99d7440743bf5faf500e02d7e044f1db340081d
foldsom <ugylokuka@gmail.com>
2026-02-02 13:22:33 +0100

    gbfs

--- backend/Models/RestaurantImage.cs
+++ backend/Models/RestaurantImage.cs
@@ -3,6 +3,6 @@
     public int Id { get; set; }
     public string ImageUrl { get; set; }
 
-    public int RestaurantId { get; set; }
+    public int RestaurantId { get; set; } //
     public Restaurant Restaurant { get; set; }
 }
