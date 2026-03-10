8e3d153ce0783094af6c6091d0ce250a2b8981a3
foldsom <90392714+foldsom@users.noreply.github.com>
2026-03-10 13:03:49 +0100

    Add listMapRestaurants function to fetch map data

--- etteremkereso-frontend/src/api/restaurants.js
+++ etteremkereso-frontend/src/api/restaurants.js
@@ -3,5 +3,8 @@ import { api } from "./client";
 export const listRestaurants = (params) =>
   api.get("/restaurants", { params }).then((r) => r.data);
 
+export const listMapRestaurants = (params) =>
+  api.get("/restaurants/map", { params }).then((r) => r.data);
+
 export const getRestaurant = (id) =>
   api.get(`/restaurants/${id}`).then((r) => r.data);
