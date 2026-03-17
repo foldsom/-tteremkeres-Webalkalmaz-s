f9b13b6079072c1ee76f57c4c0c0fa1574466f0a
foldsom <90392714+foldsom@users.noreply.github.com>
2026-03-17 21:28:31 +0845

    Rename and update review API functions

--- etteremkereso-frontend/src/api/reviews.js
+++ etteremkereso-frontend/src/api/reviews.js
@@ -1,9 +1,7 @@
 import { api } from "./client";
 
-export const getRestaurantReviews = (restaurantId) =>
+export const listReviewsForRestaurant = (restaurantId) =>
   api.get(`/reviews/restaurant/${restaurantId}`).then((r) => r.data);
 
-export const upsertReview = (restaurantId, payload) =>
-  api.post(`/reviews/${restaurantId}`, payload).then((r) => r.data);
-
-export const deleteReview = (restaurantId) => api.delete(`/reviews/${restaurantId}`).then((r) => r.data);
+export const upsertReview = (restaurantId, rating, comment) =>
+  api.post(`/reviews/${restaurantId}`, { rating, comment }).then((r) => r.data);
