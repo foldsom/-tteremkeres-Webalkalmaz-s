11d7e3764a9841cd0e2d7bda295d9f75b12a187a
azsdup <90392506+azsdup@users.noreply.github.com>
2026-03-18 11:34:01 +0100

    Add function to upload images for a restaurant

--- etteremkereso-frontend/src/api/images.js
+++ etteremkereso-frontend/src/api/images.js
@@ -2,3 +2,6 @@ import { api } from "./client";
 
 export const listImagesForRestaurant = (restaurantId) =>
   api.get(`/images/restaurant/${restaurantId}`).then((r) => r.data);
+
+export const addImageToRestaurant = (restaurantId, payload) =>
+  api.post(`/images/restaurant/${restaurantId}`, payload).then((r) => r.data);
