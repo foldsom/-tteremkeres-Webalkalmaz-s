4567a86cf07ee5d3bf8c9ac47deb2f5efb3fab2e
Villas Attila <villasattilavalentin@gmail.com>
2026-03-23 23:44:10 +0100

    Move AllowedValuesAttribute to Validation and add middleware

---
+++ backend/RestaurantFinder.Api/Middleware/GlobalExceptionHandler.cs
@@ -0,0 +1,20 @@
+﻿(function() {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('GlobalExceptionHandler', GlobalExceptionHandler);
+
+    GlobalExceptionHandler.$inject = ['$location'];
+
+    function GlobalExceptionHandler($location)
+    {
+        /* jshint validthis:true */
+        var vm = this;
+        vm.title = 'GlobalExceptionHandler';
+
+        activate();
+
+        function activate() { }
+    }
+})();
---
+++ backend/RestaurantFinder.Api/Validation/AllowedValuesAttribute.cs
@@ -0,0 +1,20 @@
+﻿(function() {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('AllowedValuesAttribute', AllowedValuesAttribute);
+
+    AllowedValuesAttribute.$inject = ['$location'];
+
+    function AllowedValuesAttribute($location)
+    {
+        /* jshint validthis:true */
+        var vm = this;
+        vm.title = 'AllowedValuesAttribute';
+
+        activate();
+
+        function activate() { }
+    }
+})();
